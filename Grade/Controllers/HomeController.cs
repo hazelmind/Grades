9684d37e0c85d62437a003b83d15c33df3c81e0f
hzlmnd <52181893+hazelmind@users.noreply.github.com>
2019-11-18 18:01:27 -0500

    MVC for user input to be converted into percentage

---
+++ Grade/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Grade.Models;
+
+namespace Grade.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        //could add bool here if there was a curve on text
+        public IActionResult FindGrade(UserInput userInput)
+        {
+            decimal tPoints = userInput.TotalPoints;
+            decimal tCorrect = userInput.TotalCorrect;
+            decimal gradeDec = tCorrect / tPoints;
+            decimal gradePer= gradeDec * 100;
+            return View(gradePer);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Grade/Models/UserInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grade.Models
+{
+    public class UserInput
+    {
+        public decimal TotalPoints { get; set; }
+        public decimal TotalCorrect { get; set; }
+    }
+    
+}
